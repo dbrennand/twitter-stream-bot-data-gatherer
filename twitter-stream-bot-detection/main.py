edd7d38e89a6a6fed7906f25e45c9115dea04023
dbrennand <52419383+dbrennand@users.noreply.github.com>
2022-02-26 17:03:13 +0000

    refactor: Add LICENSE to the top.
    Format using black.

--- twitter-stream-bot-detection/main.py
+++ twitter-stream-bot-detection/main.py
@@ -1,3 +1,27 @@
+"""
+MIT License
+
+Copyright (c) 2022 Daniel Brennand
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import argparse
 import json
 import sqlite3
@@ -5,13 +29,16 @@ import botometer
 import tweepy
 import logging
 
+
 class CustomStreamListener(tweepy.StreamListener):
     """Subclass to handle Twitter stream and sending accounts to Botometer for analysis.
 
     https://docs.tweepy.org/en/v3.10.0/streaming_how_to.html#streaming-with-tweepy
     """
 
-    def __init__(self, bom: botometer.Botometer, con: sqlite3.Connection, cur: sqlite3.Cursor):
+    def __init__(
+        self, bom: botometer.Botometer, con: sqlite3.Connection, cur: sqlite3.Cursor
+    ):
         self.bom = bom
         self.con = con
         self.cur = cur
@@ -37,35 +64,67 @@ class CustomStreamListener(tweepy.StreamListener):
         Args:
             status (tweepy.Status): The tweepy.Status model representation of a Tweet.
         """
-        print(f"Tweet received. Sending Twitter user: {status.user.screen_name} to Botometer for analysis.")
+        print(
+            f"Tweet received. Sending Twitter user: {status.user.screen_name} to Botometer for analysis."
+        )
         # Query the Botometer API for the Twitter user's bot scores
         result = self.bom.check_account(status.user.id)
         print("Storing results in database.")
         # Store the Twitter user's screen name and JSON payloads of the Tweet and Botometer API results
-        self.cur.execute("INSERT INTO data values (?, ?, ?)", [status.user.screen_name, json.dumps(status._json), json.dumps(result)])
+        self.cur.execute(
+            "INSERT INTO data values (?, ?, ?)",
+            [status.user.screen_name, json.dumps(status._json), json.dumps(result)],
+        )
         self.con.commit()
+
 
 if __name__ == "__main__":
     try:
-        parser = argparse.ArgumentParser(description="Watch the Twitter stream, sending accounts to Botometer for analysis and store the results.")
+        parser = argparse.ArgumentParser(
+            description="An application to watch the Twitter stream and send accounts to Botometer for analysis. The results are stored in a SQLite database."
+        )
         parser.add_argument("rapidapi_key", type=str, help="Botometer Rapid API key.")
-        parser.add_argument("twitter_app_auth", type=json.loads, help="Twitter application credentials.")
-        parser.add_argument("-t", "--track", action="append", help="A list of hashtags to track.")
-        parser.add_argument("-f", "--database_name", type=str, default="twitter-stream-bot-detection", help="Name of the database file. Defaults to: twitter-stream-bot-detection.")
-        parser.add_argument("-d", "--debug", action='store_true', help="Enable debug messages.")
+        parser.add_argument(
+            "twitter_app_auth", type=json.loads, help="Twitter application credentials."
+        )
+        parser.add_argument(
+            "-t", "--track", action="append", help="A list of hashtags to track."
+        )
+        parser.add_argument(
+            "-f",
+            "--database_name",
+            type=str,
+            default="twitter-stream-bot-detection",
+            help="Name of the database file. Defaults to: twitter-stream-bot-detection.",
+        )
+        parser.add_argument(
+            "-d", "--debug", action="store_true", help="Enable debug messages."
+        )
         args = parser.parse_args()
         if args.debug:
             logging.basicConfig(level=logging.DEBUG)
         # Initialise DB and table
         con = sqlite3.connect(f"{args.database_name}T{'-'.join(args.track)}.db")
         cur = con.cursor()
-        cur.execute("CREATE TABLE data (screen_name TEXT, status_json json, botometer_json json)")
+        cur.execute(
+            "CREATE TABLE data (screen_name TEXT, status_json json, botometer_json json)"
+        )
         con.commit()
         # Initialise Botometer
-        bom = botometer.Botometer(wait_on_ratelimit=True, rapidapi_key=args.rapidapi_key, **args.twitter_app_auth)
+        bom = botometer.Botometer(
+            wait_on_ratelimit=True,
+            rapidapi_key=args.rapidapi_key,
+            **args.twitter_app_auth,
+        )
         # Initialise Tweepy OAuth handler
-        auth = tweepy.OAuthHandler(consumer_key=args.twitter_app_auth["consumer_key"], consumer_secret=args.twitter_app_auth["consumer_secret"])
-        auth.set_access_token(key=args.twitter_app_auth["access_token"], secret=args.twitter_app_auth["access_token_secret"])
+        auth = tweepy.OAuthHandler(
+            consumer_key=args.twitter_app_auth["consumer_key"],
+            consumer_secret=args.twitter_app_auth["consumer_secret"],
+        )
+        auth.set_access_token(
+            key=args.twitter_app_auth["access_token"],
+            secret=args.twitter_app_auth["access_token_secret"],
+        )
         # Initialise Tweepy API
         api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
         # Initialise custom stream listener
